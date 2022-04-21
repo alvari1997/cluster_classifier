74e276cc61d124b79e5813d6ede635fba55965f6
alvari1997 <83447316+alvari1997@users.noreply.github.com>
2022-04-21 15:53:56 +0300

    Add files via upload

--- train_bboxnet.py
+++ train_bboxnet.py
@@ -11,9 +11,15 @@ from pointnet.dataset import LidarDataset, BoxDataset
 from pointnet.box_model import BoxNet
 import torch.nn.functional as F
 from tqdm import tqdm
+import numpy as np
 import matplotlib.pyplot as plt
 import time
+from model_utils import FrustumPointNetLoss, parse_output_to_tensors, huber_loss, get_box3d_corners, get_box3d_corners_helper
 
+Loss = FrustumPointNetLoss()
+NUM_HEADING_BIN = 12
+NUM_SIZE_CLUSTER = 3 #8 # one cluster for each type
+NUM_OBJECT_POINT = 512
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -126,9 +132,9 @@ for epoch in range(opt.nepoch):
         hot1 = torch.zeros(opt.batchSize)
         hot1[target == 0] = 1
         hot2 = torch.zeros(opt.batchSize)
-        hot2[target == 1] = 1
+        hot2[target == 2] = 1
         hot3 = torch.zeros(opt.batchSize)
-        hot3[target == 2] = 1
+        hot3[target == 1] = 1
         one_hot = torch.vstack((hot1, hot2, hot3))
         one_hot = one_hot.transpose(1, 0)
 
@@ -137,32 +143,85 @@ for epoch in range(opt.nepoch):
         #points, target, bbox_target, one_hot = points.cpu(), target.cpu(), bbox_target.cpu(), one_hot.cpu()
         optimizer.zero_grad()
         classifier = classifier.train()
+
+        # NN
         box_pred, center_delta = classifier(points, one_hot)
         
         # transform box_pred vector to box coordinates
         # (32, 39) -> (32, 7)
-        center_boxnet = box_pred[:, :3]
+        center_boxnet, \
+        heading_scores, heading_residual_normalized, heading_residual, \
+        size_scores, size_residual_normalized, size_residual = \
+                parse_output_to_tensors(box_pred)
+
         box3d_center = center_boxnet + center_delta
+        stage1_center = center_delta
 
-        '''loss = F.cross_entropy(pred[:len(data[0])], target)
-        #loss = F.nll_loss(pred, target)
+        # heading_scores (32, 12) which bin is the heading
+        # heading_residual (32, 12) residual angle
+        # size_scores (32, 3) which bin is the size
+        # size_residual (32, 3, 3) residual size
+
+        '''
+        2.Center
+        center: torch.Size([32, 3]) torch.float32
+        stage1_center: torch.Size([32, 3]) torch.float32
+        center_label:[32,3]
+        3.Heading
+        heading_scores: torch.Size([32, 12]) torch.float32
+        heading_residual_snormalized: torch.Size([32, 12]) torch.float32
+        heading_residual: torch.Size([32, 12]) torch.float32
+        heading_class_label:(32)
+        heading_residual_label:(32)
+        4.Size
+        size_scores: torch.Size([32, 8]) torch.float32
+        size_residual_normalized: torch.Size([32, 8, 3]) torch.float32
+        size_residual: torch.Size([32, 8, 3]) torch.float32
+        size_class_label:(32)
+        size_residual_label:(32,3)'''
+
+        # compute GT, probably wrong setup
+        box3d_center_label = bbox_target[:,:3]
+        heading_class_label = torch.floor(((np.pi*3/2 + bbox_target[:,6]) * 12 / (2*np.pi))) # maybe  -pi*3/2 < float < pi*1/2   ->   0 < int < 12
+        heading_residual_label = bbox_target[:,6] # residual of above conversion?
+        size_class_label = target
+        size_residual_label = bbox_target[:,3:6]
+
+        #ttt = torch.floor(((np.pi*3/2 + bbox_target[:,6]) * 12 / (2*np.pi)))
+
+        #print(target)
+        #print(heading_scores)
+        #print(torch.max(heading_residual_label))
+        #print(size_scores)
+        #print(size_class_label)
+
+        #print(torch.eye(12)[heading_class_label.long()])
+        #print(heading_class_label)
+        #heading_class_loss = F.nll_loss(F.log_softmax(heading_scores,dim=1), heading_class_label.long())
+
+        losses = Loss(box3d_center, box3d_center_label, stage1_center, \
+                heading_scores, heading_residual_normalized, \
+                heading_residual, \
+                heading_class_label, heading_residual_label, \
+                size_scores, size_residual_normalized, \
+                size_residual, \
+                size_class_label, size_residual_label)
+
         
-        # add bbox loss
-        # ...
+        
+        loss = losses['total_loss']
 
-        # plot loss
-        #plt.scatter(i, loss.cpu().detach().numpy())
-        #plt.pause(0.005)
+        
 
         loss.backward()
         optimizer.step()
-        pred_choice = pred.data.max(1)[1]
+        #pred_choice = pred.data.max(1)[1]
         #correct = pred_choice.eq(target.data).cpu().sum()
-        correct = pred_choice[0].eq(target.data).cpu().sum()
-        print('[%d: %d/%d] train loss: %f accuracy: %f' % (epoch, i, num_batch, loss.item(), correct.item() / float(opt.batchSize)))
-        #print('[%d: %d/%d] train loss: %f' % (epoch, i, num_batch, loss.item()))
+        #correct = pred_choice[0].eq(target.data).cpu().sum()
+        #print('[%d: %d/%d] train loss: %f accuracy: %f' % (epoch, i, num_batch, loss.item(), correct.item() / float(opt.batchSize)))
+        print('[%d: %d/%d] train loss: %f' % (epoch, i, num_batch, loss.item()))
 
-        if i % 10 == 0:
+        '''if i % 10 == 0:
             j, data = next(enumerate(testdataloader, 0))
             points, target = data
             target = target[:, 0]
@@ -175,14 +234,14 @@ for epoch in range(opt.nepoch):
             #correct = pred_choice.eq(target.data).cpu().sum()
             correct = pred_choice[0].eq(target.data).cpu().sum()
             print('[%d: %d/%d] %s loss: %f accuracy: %f' % (epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize)))
-            #print('[%d: %d/%d] %s loss: %f' % (epoch, i, num_batch, blue('test'), loss.item()))
+            #print('[%d: %d/%d] %s loss: %f' % (epoch, i, num_batch, blue('test'), loss.item()))'''
         i = i + 1
 
     torch.save(classifier.state_dict(), '%s/cls_model_%d.pth' % (opt.outf, epoch))
 
 plt.show()
 
-total_correct = 0
+'''total_correct = 0
 total_testset = 0
 for i,data in tqdm(enumerate(testdataloader, 0)):
     points, target = data
